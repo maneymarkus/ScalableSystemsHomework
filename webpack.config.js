07ccf7cee6bd085b34f4dc4ea00a9c31ce77bbd0
maneymarkus <markus.staedler@hotmail.de>
2020-05-05 23:43:27 +0200

    Tested some functionality and changed project structure slightly again

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,23 @@ module.exports = {
   module : {
     rules : [
       {
-        test : /\.s[ac]ss$/i,
-        use : [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ]
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins: function () { // postcss plugins, can be exported to postcss.config.js
+              return [
+                require('autoprefixer') //TODO: install autoprefixer maybe
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
       },
       {
         test : /\.css$/i,
