f4e0fbceb8d9d8edaf83798209416bfc86d31abd
maneymarkus <markus.staedler@hotmail.de>
2020-05-25 17:49:11 +0200

    Tasks can be marked done

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 function init(window, document, undefined) {
 
-  let Module = (function(window, document, undefined) {
+  let SliderModule = (function(window, document, undefined) {
 
     if (slider = document.getElementById("myRange")) {
       var output = document.getElementById("progresspercent");
@@ -9,6 +9,32 @@ function init(window, document, undefined) {
         output.innerHTML = this.value + "%";
       }
     }
+
+  })(window, document);
+
+  let MainTodoModule = (function (window, document, undefined) {
+
+    let todos = document.getElementsByClassName("todo");
+
+    for (let i = 0; i < todos.length; i++) {
+      todos[i].addEventListener("click", function(e) {
+        let ev = e || window.event;
+        let target = ev.target || window.target;
+        while (target && target.nodeName != "A" && target.nodeName != "A") {
+          target = target.parentNode;
+        }
+        if (!target || target.nodeName == "BODY") {
+          return;
+        }
+        if (target.classList.contains("todo-done")) {
+          this.classList.add("task-done");
+        }
+        if (target.classList.contains("delete")) {
+          //TODO: react to delete
+        }
+      });
+    }
+
 
   })(window, document);
 
