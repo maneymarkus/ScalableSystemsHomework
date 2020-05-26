8ff57d6e9b5191f2b07540c11833f84e4de94c90
maneymarkus <markus.staedler@hotmail.de>
2020-05-26 16:27:20 +0200

    Added delete modal

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,7 @@ function init(window, document, undefined) {
 
   let MainTodoModule = (function (window, document, undefined) {
 
-    let todos = document.getElementsByClassName("todo");
+    let todos = $(".todo");
 
     for (let i = 0; i < todos.length; i++) {
       todos[i].addEventListener("click", function(e) {
@@ -33,8 +33,42 @@ function init(window, document, undefined) {
           //TODO: react to delete
         }
       });
+
+      return {
+        getTodosApi : function() {
+          return todos;
+        },
+      }
+
     }
 
+  })(window, document);
+
+  let ModalModule = (function(window, document, undefined) {
+
+    let deleteTodo;
+
+    $('#delete-modal').on('show.bs.modal', function () {
+      let deleteButton = $(event.relatedTarget);
+      let target = event.target;
+      while(target && target.nodeName != "BODY" && !target.classList.contains("todo")) {
+          target = target.parentNode;
+      }
+      deleteTodo = target;
+      let task = target.getElementsByClassName("task")[0].innerHTML;
+      let modal = $(this);
+      modal.find("span.task-name").text(task);
+    });
+
+    $("#delete-modal .delete-btn").on("click", function() {
+      if (deleteTodo != undefined) {
+        window.setTimeout(function() {
+          deleteTodo.remove();
+          deleteTodo = undefined;
+        }, 500);
+      }
+      $("#delete-modal").modal("hide");
+    });
 
   })(window, document);
 
